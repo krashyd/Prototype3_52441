798a9fbd47054637537d9e6489f641301ef1c5e1
krashyd <a52441@alunos.ipb.pt>
2024-04-29 12:47:12 +0100

    Tutorial Lesson 3.2 - Make the World Whiz By
    a52441

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,16 +8,21 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float  startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController PlayerControllerScript;
     
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating ("SpawnObstacle", startDelay, repeatRate); 
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void SpawnObstacle()
     {
+        if(PlayerControllerScript.gameOver == false)
+        {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 
     // Update is called once per frame
