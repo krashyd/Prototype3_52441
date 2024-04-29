e3337bc25d711dc7e5fed69316de36a09d4a9a6d
krashyd <a52441@alunos.ipb.pt>
2024-04-29 12:29:35 +0100

    Spawn obstacles at intervals
    a52441

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,11 +6,18 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float  startDelay = 2;
+    private float repeatRate = 2;
     
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);    
+        InvokeRepeating ("SpawnObstacle", startDelay, repeatRate); 
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 
     // Update is called once per frame
