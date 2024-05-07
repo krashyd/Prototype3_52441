b90a7e8446790a181fdc0fef0d427995922d6f77
krashyd <a52441@alunos.ipb.pt>
2024-05-07 15:11:12 +0100

    fix some problems , and keep the player without jumping when dead
    a52441

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
-    private PlayerController PlayerControllerScript;
+    private PlayerController playerControllerScript;
     private float leftBound = -15;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(PlayerControllerScript.gameOver == false);
+        if(playerControllerScript.gameOver == false)
         {
         transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
+        
         if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
